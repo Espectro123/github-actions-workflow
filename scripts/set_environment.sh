ac89d524abe3b5317edd3e770c7496cb0043adbf
Enrique <kicalin@protonmail.com>
2024-09-27 18:53:52 +0200

    I give up 4

--- scripts/set_environment.sh
+++ scripts/set_environment.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-environment=""
-
 if [[ "$1" = "refs/heads/main" ]]; then
-    environment="environment=prod"
-    echo "prod"
+    echo "environment=prod" >> $2
+    echo "tf_vars_file=prod.tfvars" >> $2
+    
 elif [[ "$1" = "refs/heads/dev" ]]; then
-    environment="environment=dev"
-    echo "dev"
+    echo "environment=dev" >> $2
+    echo "tf_vars_file=dev.tfvars" >> $2
 fi
 
-echo $environment
